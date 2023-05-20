af7316c71ffb9f24f54600078f75a81a7f5df6e3
nirjhor saha <nirjhor.badhan25@gmail.com>
2023-05-21 03:26:13 +0600

    delete toy info

--- index.js
+++ index.js
@@ -78,6 +78,8 @@ async function run() {
       const result = await jobsCollection.deleteOne(query);
       res.send(result);
     });
+
+    
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
